d82651bffed5522608bc90debb20d668993060b3
Js-project <le7435027@gmail.com>
2026-09-15 21:39:38 +0700

    da xong lab1

---
+++ notes/lab1.js
@@ -0,0 +1,11 @@
+console.log("Đây là lab 1");
+
+let name = "Lê Trung Hiếu";
+const age = 22;
+const address = "Hà Nội";
+const status = "Sinh viên";
+
+console.log("Tên tôi là:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Trạng thái:", status);
