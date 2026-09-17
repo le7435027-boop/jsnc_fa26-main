ef4b5115fcbe44703218887de91147004112b39f
Js-project <le7435027@gmail.com>
2026-09-17 15:32:21 +0700

    Cap nhat bai lab1

---
+++ lab1.js
@@ -0,0 +1,35 @@
+console.log("Đây là lab 1");
+
+let name = "Lê Trung Hiếu";
+let age = 22;
+const address = "Hà Nội";
+const status = "Sinh viên";
+
+console.log("Tên tôi là:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Trạng thái:", status);
+
+// bai 2
+let tuoi = 20;
+console.log(tuoi);
+tuoi = 21;
+console.log(tuoi);
+
+
+//bai 3
+const a = 10;
+const b = 5;
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+
+//bai 4
+function sayHello(name) {
+    console.log("Xin chào ",name);
+}
+sayHello("An");
+sayHello("Bình");
+sayHello("Hiếu");
